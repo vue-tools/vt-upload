f18915fc36a84b19cdc1f6428e71dd9efa8113e8
Jiangtao <321jiangtao@gmail.com>
2017-11-24 15:33:44 +0800

    build: 1.0.5

--- dist/index.vue
+++ dist/index.vue
@@ -5,7 +5,6 @@
 <script>
     import Vue from 'vue'
     import Imager from 'Imager'
-
     export default {
         props: {
             url: {
@@ -33,22 +32,18 @@
         methods: {
             change(e) {
                 let file, image
-
                 file = e.target.files[0]
-
                 if (!file) {
                     return
                 }
-
+                e.target.value = ''
                 this.$emit('change', e)
-
                 if (Imager.isImage(file) && this.compress) {
                     image = new Imager({
                         file: file,
                         quality: 0.8,
                         isLimit: this.limit
                     })
-
                     image.getExif().then(data => image.loaded()).then(base64 => {
                         if (this.base64) {
                             this.upload(base64, base64)
@@ -56,14 +51,15 @@
                             this.upload(Imager.base64ToFile(base64, file.name), base64)
                         }
                     })
+                } else if(this.base64 && Imager.isImage(file)){
+                    Imager.fileToBase64(file).then(base64 => this.upload(base64, base64))
                 } else {
-                    Imager.fileToBase64(file).then(base64 => this.upload(file, base64))
+                    this.upload(file, null)
                 }
             },
             upload(file, base64) {
                 if (this.url) {
                     let fd = new FormData()
-
                     fd.append(this.name, file)
                     Vue.http.post(this.url, fd, {
                         progress: (e) => {
